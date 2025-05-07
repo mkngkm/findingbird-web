00ef8a723ed91d727f07737e56cb194a1650d8d2
mkngkm <mikang1031@gmail.com>
2025-05-08 01:17:07 +0900

    feat: 로그인 시 redirect and 로그인 ui

--- findingbird-web/src/app/page.tsx
+++ findingbird-web/src/app/page.tsx
@@ -1,8 +1,21 @@
-import Image from "next/image";
-import Logo from '@/app/assets/img/Logo.png';
-import LoginForm from "./ui/components/auth/login-form";
+'use client';
 
-export default function Home() {
+import { useEffect } from 'react';
+import Cookies from 'js-cookie';
+import { useRouter } from 'next/navigation';
+import LoginForm from './ui/components/auth/login-form';
+import Image from 'next/image';
+
+export default function RootPage() {
+  const router = useRouter();
+
+  useEffect(() => {
+    const token = Cookies.get('token');
+    if (token) {
+      router.replace('/home');
+    }
+  }, [router]);
+
   return (
     <div className='px-10 mb-10 flex flex-col items-center'>
       <div className="mt-8 w-full">
