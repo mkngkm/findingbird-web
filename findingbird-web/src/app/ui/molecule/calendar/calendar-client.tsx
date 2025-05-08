e111e090c22b6086f483a857fbc6b52925c30014
CSyebin <yebin1230@korea.ac.kr>
2025-05-09 04:19:35 +0900

    style: record | 미세한 디자인 조정
    상단 기록 | 도감 컬러와 기록 | 지난 목표 컬러를 서로 바꿈
    기록 | 지난 목표 폰트 사이즈 줄임

--- findingbird-web/src/app/ui/molecule/calendar/calendar-client.tsx
+++ findingbird-web/src/app/ui/molecule/calendar/calendar-client.tsx
@@ -84,8 +84,7 @@ export default function CalendarClient() {
   return (
     <div className="relative pb-20"> {/* 플로팅 버튼 공간 확보 */}
       {/* 상단 연/월 변경 */}
-      <div className="flex justify-between items-center w-full max-w-md mb-4 px-4">
-        <button onClick={() => changeMonth(-1)}>&lt;</button>
+      <div className="flex justify-between items-center w-full max-w-md mb-4 px-4 text-2xl font-semibold">        <button onClick={() => changeMonth(-1)}>&lt;</button>
         <CalendarModal year={year} month={month} setYear={setYear} setMonth={setMonth} />
         <button onClick={() => changeMonth(1)}>&gt;</button>
       </div>
@@ -115,13 +114,13 @@ export default function CalendarClient() {
       {/* 기록/목표 탭 */}
       <div className="mt-6 flex justify-center space-x-4">
         <button
-          className={`px-4 py-2 rounded-full ${selectedTab === '기록' ? 'bg-birdGreen600 text-white' : 'bg-gray-200'}`}
+          className={`px-4 py-2 text-sm rounded-full ${selectedTab === '기록' ? 'bg-birdGreen400 text-white' : 'bg-gray-200'}`}
           onClick={() => setSelectedTab('기록')}
         >
           기록
         </button>
         <button
-          className={`px-4 py-2 rounded-full ${selectedTab === '지난 목표' ? 'bg-birdGreen600 text-white' : 'bg-gray-200'}`}
+          className={`px-4 py-2 text-sm rounded-full ${selectedTab === '지난 목표' ? 'bg-birdGreen400 text-white' : 'bg-gray-200'}`}
           onClick={() => setSelectedTab('지난 목표')}
         >
           지난 목표
