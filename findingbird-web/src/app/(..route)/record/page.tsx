7d2b38b2b292c6a812fbf64e9cc975126cb76076
mkngkm <mikang1031@gmail.com>
2025-05-08 23:58:44 +0900

    feat: 도감 record에 import

--- findingbird-web/src/app/(..route)/record/page.tsx
+++ findingbird-web/src/app/(..route)/record/page.tsx
@@ -4,6 +4,7 @@ import { useState } from "react";
 import TextToggleButton from "@/app/ui/atom/toggle/text-toggle-button";
 import Encyclopedia from "@/app/ui/components/record/encyclopedia";
 import CalendarClient from "@/app/ui/molecule/calendar/calendar-client";
+import Collection from "@/app/ui/components/record/collection";
 
 export default function RecordPage() {
   const [showEncyclopedia, setShowEncyclopedia] = useState(false);
@@ -15,7 +16,7 @@ export default function RecordPage() {
           <div className="flex justify-center mb-6">
             <TextToggleButton text="기록/도감" onToggle={(active) => setShowEncyclopedia(active)} />
           </div>
-          {!showEncyclopedia ? <CalendarClient /> : <Encyclopedia />}
+          {!showEncyclopedia ? <CalendarClient /> : <Collection />}
         </div>
       </div>
     </main>
